a8ba2ccad7d2136265227935796f1b154285d7cd
NickMikhailyuk <captain.sevastopol@gmail.com>
2016-03-06 15:27:25 +0300

    first JSP + login form (without css)

--- src/main/java/com/ms/login/LoginServlet.java
+++ src/main/java/com/ms/login/LoginServlet.java
@@ -13,9 +13,25 @@ public class LoginServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
-        
+	    String username = request.getParameter("username");
+	    String password = request.getParameter("password");
         PrintWriter pw = response.getWriter();
-        pw.println("<H1>Hello, world! или Привет мир</H1>");
+        pw.println(              
+        		
+        		"<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" +" +
+                "http://www.w3.org/TR/html4/loose.dtd\">\n" +
+            "<html> \n" +
+              "<head> \n" +
+                "<meta http-equiv=\"Content-Type\" content=\"text/html; " +
+                  "charset=ISO-8859-1\"> \n" +
+                "<title> Message Service  </title> \n" +
+              "</head> \n" +
+              "<body> <div align='center'> \n" +
+                "<style= \"font-size=\"12px\" color='black'\"" + "\">" +
+                  "Username: " + username + " <br> " + 
+                  "Password: " + password +
+              "</font></body> \n" +
+            "</html>");
 	}
 }
 
